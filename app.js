730c7fe5695a1be2988d0c2d66fc1f550ac2cace
Syed Karnain Kamal <hasansyed700@gmail.com>
2023-02-17 10:23:28 +0500

    arrey practice

---
+++ app.js
@@ -0,0 +1,47 @@
+ //un-shift //
+  
+// var fruitName = ["apple","orange"]
+// var addFruitName = fruitName.unshift(prompt("add fruit"))
+
+// console.log(fruitName)
+// console.log(addFruitName)
+ 
+ //shift //
+
+// var addFruitName = fruitName.shift(prompt("add fruit"))
+
+// console.log(fruitName)
+// console.log(addFruitName)
+
+ //push //
+  
+// var fruitName = ["apple","orange"]
+// var addFruitName = fruitName.push(prompt("add fruit"))
+
+// console.log(fruitName)
+// console.log(addFruitName)
+ 
+ //pop//
+
+//  var addFruitName = fruitName.pop(prompt("add fruit"))
+ 
+//  console.log(fruitName)
+//  console.log(addFruitName)
+ 
+// SPLICE // 
+
+  
+// var fruitName = ["apple","orange"]
+// var addFruitName = fruitName.splice(2, 0 ,"pineapple","banana")
+
+// console.log(fruitName)
+// console.log(addFruitName)
+ 
+// slice //
+
+// var fruitName = ["apple","orange", "pineapple", "banana","strawberry"]
+// var addFruitName = fruitName.slice(2,4)
+
+// console.log(fruitName)
+// console.log(addFruitName)
+ 
